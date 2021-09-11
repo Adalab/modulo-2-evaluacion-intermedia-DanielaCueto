775e484ad9df5ef116d86bced38e158d50cf39ed
Daniela <danisalazarcueto7@gmail.com>
2021-09-11 13:47:31 +0100

    changes to improve the exercise: done

--- JS/main.js
+++ JS/main.js
@@ -1,37 +1,36 @@
 "user strict";
 
-//constantes
 const boxNumber = document.querySelector(".js-boxNumber");
 const tryButton = document.querySelector(".js-button");
 const hintText = document.querySelector(".js-hint");
 const numAttemptsText = document.querySelector(".js-attempts");
 let numAttempts = 0;
 
-//funciones
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 const randomNumber = getRandomNumber(99) + 1;
-console.log(randomNumber);
 
-function handleAttempt() {
+function handleAttempt(ev) {
+  ev.preventDefault();
+  giveFeedBack();
+  updateAttempts();
+}
+function giveFeedBack() {
   const guessValue = parseInt(boxNumber.value);
-  //console.log(guessValue);
-  if (guessValue === randomNumber) {
+  if (guessValue < 1 || guessValue > 100) {
+    hintText.innerHTML = "El número es inválido";
+  } else if (guessValue === randomNumber) {
     hintText.innerHTML = "¡¡Has ganado campeona!!";
   } else if (guessValue < randomNumber) {
     hintText.innerHTML = "Demasiado bajo";
   } else {
     hintText.innerHTML = "Demasiado alto";
   }
+}
+function updateAttempts() {
   numAttempts = numAttempts + 1;
   numAttemptsText.innerHTML = numAttempts;
 }
 
-//eventos
 tryButton.addEventListener("click", handleAttempt);
-
-// que me piden:
-// generar un número aleatorio entre el 1 y el 100
-// pistas sobre si el numero que prueba esta demasiado alto o bajo
-// contabiliza el numero de intentos que he ha introduzido
